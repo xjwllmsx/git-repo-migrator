faf790103a114594678b87532e8a04043e1beec3
Joseph <github@mail.joseph-williams.me>
2024-11-19 00:38:24 -0600

    Replace print statements with log statements

--- RemoteServer.py
+++ RemoteServer.py
@@ -4,6 +4,9 @@ import os
 # Import subprocess
 import subprocess
 
+# Import logging
+import logging
+
 
 # RemoteServer Class
 class RemoteServer:
@@ -13,21 +16,25 @@ class RemoteServer:
 
     # Prints the current remote servers
     def check_remote_server(self):
-        print(f"Current remote servers for: '{self.repo_name}'")
-
-        # os.system(f"cd {self.repo_path} && git remote -v")
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            filename="test.log",
+        )
+        logging.info(f"Current remote servers for: '{self.repo_name}'")
 
         # Saves terminal output to a str variable
         self.output = subprocess.getoutput(f"cd {self.repo_path} && git remote -v")
 
         if "remote_mirror" in self.output:
-            print("This repo has a mirror")
-            print(self.output)
+            logging.info("This repo has a mirror")
+            logging.info(f"{self.output}\n")
         elif "origin" in self.output:
-            print("This repo already has a remote")
-            print(self.output)
+            logging.info("This repo already has a remote")
+            logging.info(f"{self.output}\n")
         elif "" in self.output:
-            print("This repo has no current remotes")
+            logging.info("This repo has no current remotes\n")
 
     # Removes remote server from repo
     def remove_remote_server(self):
--- migrate_repos.py
+++ migrate_repos.py
@@ -7,6 +7,15 @@ from create_repo_dict import create_repo_dict
 # Import RemoteServer class
 from RemoteServer import RemoteServer
 
+# Import logging
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename="test.log",
+)
+
 # New RootDir object
 root_dir = RootDir()
 
@@ -17,17 +26,17 @@ repos_dict = create_repo_dict(root_dir.root_dir_path, root_dir.root_sub_dirs)
 num_of_repos = root_dir.num_of_subs
 
 # Print that the program is about to begin
-print("The program is now starting...")
+logging.info("The program is now starting...\n")
 
 # Print navigating to the selected directory
-print(f"Migrating repositories from '{root_dir.root_dir_folder}'...")
+logging.info(f"Migrating repositories from '{root_dir.root_dir_folder}'...\n")
 
 # Print number of repos to migrate
-print(f"There are a total of {num_of_repos} repos to migrate.")
+logging.info(f"There are a total of {num_of_repos} repos to migrate.\n")
 
 for key in repos_dict:
     repo_name = repos_dict[key]["repo_name"]
     repo_path = repos_dict[key]["repo_path"]
     remote = RemoteServer(repo_name, repo_path)
     remote.check_remote_server()
-    print("\n")
+    # print("\n")
