09478769c256c3ae85a5c33a19dbf5fe4ccfb7f7
Joseph <github@mail.joseph-williams.me>
2024-11-21 23:35:57 -0600

    Create print_subdirs_list and remove_subdir functions, and conditional statements to update subdirectory list

--- main.py
+++ main.py
@@ -13,19 +13,42 @@ root_dir = Repo()
 # Initialize the root_dir
 set_root_dir(root_dir)
 
+# Create a list of subdirectories
 list = root_dir.create_subdirs_list()
 
 
-# def print_subdirs_list(list):
+# Print list of subdirectories
+def print_subdirs_list(list):
+    counter = 1
+    for obj in list:
+        print(f"[{counter}] {obj.repo_name}")
+        print(f"Path: {obj.dir_path}\n")
+        counter += 1
 
 
-for obj in list:
-    print(f"\n{obj.repo_name}:")
-    print(obj.dir_path)
+# Removes indicated subdirectory
+def remove_subdir(list, num):
+    list.pop(num - 1)
 
-print(len(list))
-popped_item = list.pop(3)
-print(len(list))
-print(popped_item.repo_name)
 
-# print(list[4-4].repo_name)
+print("\n")
+print_subdirs_list(list)
+confirm_subdirs = input(
+    "Would you like to remove any of the listed subdirectories? \n[y/n]:"
+)
+
+if confirm_subdirs == "n" or confirm_subdirs == "y":
+    if confirm_subdirs == "y":
+        rm_subdir = input(
+            f"Which subdirectory would you like to remove from the list? Enter a number between 1 - {len(list)}: "
+        )
+        if rm_subdir.isnumeric():
+            remove_subdir(list, int(rm_subdir))
+        else:
+            print("Try again!")
+    elif confirm_subdirs == "n":
+        pass
+
+
+# remove_subdir(list, 12)
+print_subdirs_list(list)
